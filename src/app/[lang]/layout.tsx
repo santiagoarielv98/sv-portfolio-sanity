be94c9f54685835f8637acc61b12de997620143d
Santiago Ariel <santiagoarielv98@gmail.com>
2025-02-20 22:32:31 -0300

    feat: refactor layout structure by consolidating theme provider and metadata generation, remove redundant layout file

--- src/app/[lang]/layout.tsx
+++ src/app/[lang]/layout.tsx
@@ -1,6 +1,43 @@
 import type { Locale } from "@/lib/i18n/config";
 import { i18n } from "@/lib/i18n/config";
+import { Space_Grotesk, Urbanist } from "next/font/google";
+
 import "./globals.css";
+import { sanityFetch } from "@/sanity/lib/live";
+import { settingQuery } from "@/sanity/lib/queries";
+import type { Metadata } from "next";
+import { ThemeProvider } from "./provider";
+
+const spaceGrotesk = Space_Grotesk({
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-space",
+});
+
+const urbanist = Urbanist({
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-urbanist",
+});
+
+type Props = {
+  params: Promise<{
+    lang: Locale;
+  }>;
+};
+
+export async function generateMetadata({ params }: Props) {
+  const { data } = await sanityFetch({
+    query: settingQuery,
+    params,
+  });
+  return {
+    title: data?.title,
+    description: data?.description as unknown as string,
+    keywords: data?.keywords,
+    authors: [{ name: data!.author! }],
+  } satisfies Metadata;
+}
 
 export async function generateStaticParams() {
   return i18n.locales.map((locale) => ({ lang: locale }));
@@ -11,13 +48,24 @@ export default async function RootLayout({
   params,
 }: Readonly<{
   children: React.ReactNode;
-  params: Promise<{ lang: Locale }>;
-}>) {
+}> &
+  Props) {
   const { lang } = await params;
 
   return (
     <html lang={lang} suppressHydrationWarning>
-      <body>{children}</body>
+      <body
+        className={`${spaceGrotesk.variable} ${urbanist.variable} font-urbanist min-h-screen`}
+      >
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
